a962c680af8ae0472b59cfe67add681925965480
Reales Myles <rclarence123@gmail.com>
2023-05-14 10:52:39 -0500

    Rutas y handlers

--- introduccion/goweb.go
+++ introduccion/goweb.go
@@ -6,10 +6,19 @@ import (
 	"net/http"
 )
 
+// Handlers
+func Hola(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "Hola Mundo")
+}
+
 func main() {
+
+	//Router
+	http.HandleFunc("/", Hola)
+
 	//Crear servidor
 
-	fmt.Println("El servidor esa coorriendo en el puerto 3000")
+	fmt.Println("El servidor esta coorriendo en el puerto 3000")
 	fmt.Printf("Run Server: http://localhost:3000/")
 	log.Fatal(http.ListenAndServe("localhost:3000", nil))
 }
